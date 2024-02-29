c1c2d3f2c53264499a3151fdbc8827973d5d3be7
fceja <88167774+fceja@users.noreply.github.com>
2024-02-29 13:17:25 -0800

    add start docker container

--- StartDockerContainer.ts
+++ StartDockerContainer.ts
@@ -1,7 +1,12 @@
+import { exec } from "child_process";
 import Dockerode from "dockerode";
 
+const containerName = "my-postgres-container";
+const imageName = "postgres";
+const postgresPass = "POSTGRES_PASSWORD=mysecret";
+
 /* initalize a docker postgres database instance */
-export const startDockerContainer = async () => {
+export const createDockerContainer = async () => {
   // create docker client
   const docker = new Dockerode();
 
@@ -10,12 +15,55 @@ export const startDockerContainer = async () => {
 
   // create container
   const container = await docker.createContainer({
-    Image: "postgres",
-    name: "my-postgres-container",
-    Env: ["POSTGRES_PASSWORD=mysecret"],
+    Image: imageName,
+    name: containerName,
+    Env: [postgresPass],
     HostConfig: {},
   });
   await container.start();
 
-  console.log("...postgres container started successfully");
+  console.log("...postgres created successfully");
+};
+
+const checkContainerExists = () => {
+  return new Promise((resolve, reject) => {
+    exec('docker ps -a --format "{{.Names}}"', (error, stdout) => {
+      if (error) {
+        reject(error);
+      }
+
+      const containerNames = stdout.split("\n");
+      resolve(containerNames.includes(containerName));
+    });
+  });
+};
+
+const execAsync = (command: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(stdout);
+    });
+  });
+};
+
+const runContainer = async () => {
+  const result = await execAsync(`docker start ${containerName}`);
+  console.log(`runContainer result -> ${result}`);
+};
+
+/* initalize a docker postgres database instance */
+export const startDockerContainer = async () => {
+  try {
+    if (await checkContainerExists()) {
+      runContainer();
+    } else {
+      createDockerContainer();
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
