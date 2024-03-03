a78253249af953dbc313adbcfa1b568619091a47
fceja <88167774+fceja@users.noreply.github.com>
2024-03-03 12:39:14 -0800

    add db creation

--- src/helpers/postgres/Postgres.ts
+++ src/helpers/postgres/Postgres.ts
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { IPostgres } from "@helpers/postgres/PostgresTypes";
 import { Pool, QueryResult } from "pg";
 
@@ -22,23 +23,12 @@ export default class Postgres {
     this.pgPort = props.pgPort;
   }
 
-  createDbConnection = async () => {
-    this.pool = new Pool({
-      user: this.pgUser,
-      host: this.pgHost,
-      database: this.pgDefaultDatabase,
-      password: this.pgPass,
-      port: Number(this.pgPort.split(":")[0]),
-    });
-  };
-
   checkIfDbExists = async () => {
     try {
-      console.log("...checking if database exists");
       const query = `
-        SELECT datname
-        FROM pg_database;
-        `;
+          SELECT datname
+          FROM pg_database;
+          `;
 
       const { rows }: QueryResult = await this.pool.query(query);
       const databases = rows.map((row) => row.datname);
@@ -51,6 +41,25 @@ export default class Postgres {
     return false;
   };
 
+  createDbConnection = async () => {
+    this.pool = new Pool({
+      user: this.pgUser,
+      host: this.pgHost,
+      database: this.pgDefaultDatabase,
+      password: this.pgPass,
+      port: Number(this.pgPort.split(":")[0]),
+    });
+  };
+
+  createDb = async () => {
+    try {
+      const query = `CREATE DATABASE ${this.pgDatabase}`;
+      await this.pool.query(query);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   disconnectDb = async () => {
     await this.pool.end();
     this.pool = null;
@@ -58,14 +67,19 @@ export default class Postgres {
 
   initDB = async () => {
     try {
+      // initiate db connection
       await this.createDbConnection();
 
+      // check if db exists, else create
       if (await this.checkIfDbExists()) {
         console.log(`...db already exists`);
       } else {
         console.log(`...creating new db`);
+        await this.createDb();
       }
+      assert.equal(true, await this.checkIfDbExists());
 
+      // disconnect from db
       await this.disconnectDb();
     } catch (error) {
       console.error("Error connecting to Postgres:", error);
