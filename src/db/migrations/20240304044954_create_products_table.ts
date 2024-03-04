c2e8f9ef28e447408812dfc85e5734528cd11923
fceja <88167774+fceja@users.noreply.github.com>
2024-03-04 09:25:04 -0800

    add migration to create products table

---
+++ src/db/migrations/20240304044954_create_products_table.ts
@@ -0,0 +1,28 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.smallint("category_id").notNullable();
+    table.string("bar_code", 50);
+    table.string("sku_code", 50);
+    table.string("qr_code", 50);
+    table.string("name", 100).notNullable();
+    table.string("description", 200);
+    table.integer("quantity").notNullable();
+    table.smallint("weight");
+    table.smallint("height");
+    table.smallint("width");
+    table.smallint("depth");
+    table.tinyint("refrigerated").notNullable();
+    table.tinyint("fragile").notNullable();
+    table.tinyint("heavy").notNullable();
+
+    // timestamps creates `created_at` and `updated_at` columns
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists("products");
+}
