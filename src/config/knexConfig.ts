ac0552fa721ce883d1bb90fbd1814f54835b0ec8
fceja <88167774+fceja@users.noreply.github.com>
2024-03-03 21:17:21 -0800

    add knexconfig

---
+++ src/config/knexConfig.ts
@@ -0,0 +1,20 @@
+import configData from "./config.json";
+
+module.exports = {
+  development: {
+    client: configData.pgClient,
+    connection: {
+      user: configData.pgUser,
+      password: configData.pgPass,
+      host: configData.pgHost,
+      port: Number(configData.pgPort.split(":")[0]),
+      database: configData.pgDatabase,
+    },
+    migrations: {
+      directory: "../db/migrations",
+    },
+    seeds: {
+      directory: "../src/db/seeds",
+    },
+  },
+};
