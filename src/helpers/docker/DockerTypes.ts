cb4b41bda2fd6224cf74d8094ffc025662389016
fceja <88167774+fceja@users.noreply.github.com>
2024-03-11 09:05:31 -0700

    remove types, not needed

--- src/helpers/docker/DockerTypes.ts
+++
@@ -1,4 +0,0 @@
-import { TPostgresConfig } from "@helpers/postgres/PostgresTypes";
-
-export interface IDockerContainerPostgres extends TPostgresConfig {}
-export interface IDocker extends IDockerContainerPostgres {}
