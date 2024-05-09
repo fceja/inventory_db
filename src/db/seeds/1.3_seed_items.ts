dc62d60a5d435e36e85a3c7e4672c98e967df117
fceja <88167774+fceja@users.noreply.github.com>
2024-05-09 15:29:39 -0700

    update item seed data

--- src/db/seeds/1.3_seed_items.ts
+++ src/db/seeds/1.3_seed_items.ts
@@ -9,25 +9,25 @@ export async function seed(knex: Knex): Promise<void> {
       price: 0.00
     },
     {
-      parentFolderId: 1,
+      parentFolderId: 2,
       name: "Item 2 - Sticker",
       quantity: 100,
       price: 50.21
     },
     {
-      parentFolderId: 2,
+      parentFolderId: 1,
       name: "Item 3 - Limited Belt",
       quantity: 125,
       price: 300.97
     },
     {
-      parentFolderId: 3,
+      parentFolderId: 6,
       name: "Item 4 - Plain shirt",
       quantity: 25,
       price: 100.00
     },
     {
-      parentFolderId: 3,
+      parentFolderId: 6,
       name: "Item 5 - Ink",
       quantity: 25,
       price: 0.01
